144624a031b81115f120587302f1fdca87723233
devin-dunn <devineamondunn@live.com>
2023-04-03 17:10:38 -0400

    form is somewhat working

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,22 @@
 
 const Form = (props) => {
 
-    const {handleChange, handleSubmit} = props;
+    const {handleChange, handleSubmit, handleToggle, sunriseRun} = props;
+
+    const toggleDisplay = sunriseRun ? "Sunrise Run" : "Sunset Run" 
 
     return (
         <section className="form">
             <form onSubmit={handleSubmit} action="">
-                <label htmlFor="date">date</label>
-                <input onChange={handleChange} type="text" name="date" id="date" placeholder="yyyy-mm-dd" />
-                <button>Get Sun Data!</button>
+                <label htmlFor="date" className="sr-only">date</label>
+                <input onChange={handleChange} type="date" name="date" id="date" placeholder="yyyy-mm-dd" />
+                
+                <a onClick={handleToggle}>{toggleDisplay}</a>
+                <button>Get Sun Data!</button> 
+                {sunriseRun === false && <>
+                <label className="sr-only">Length of run</label>
+                <input type="numbers" placeholder="enter run length in minutes"></input>
+                </>}
             </form>
         </section>
     )
---
+++ src/components/SunDisplay.js
@@ -0,0 +1,16 @@
+
+
+const SunDisplay = (props) => {
+
+    console.log('display', props)
+
+    const {sunriseRun, sunData} = props;
+
+    const departureTime = sunriseRun ? sunData.results.sunrise : sunData.results.sunset
+
+    return(
+        <p>Start your SunRun at: <span>{departureTime}</span></p>
+    )
+}
+
+export default SunDisplay;
