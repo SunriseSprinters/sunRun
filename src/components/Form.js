51e348bf6febe9fa6e38ce494f57db957b4edd6a
chelseycornejo <chelsey.daguio@gmail.com>
2023-04-03 19:07:04 -0400

    added error handling for calendar so user cannot choose a date earlier than current date

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,21 @@
+import { useState, useEffect } from "react";
 
 const Form = (props) => {
     // de-structuring the props and using the passed down functions for onSubmit and onChange events.
     const {handleChange, handleSubmit, handleToggle, sunriseRun} = props;
 
+    const [curDate, setCurDate] = useState(Date());
     const toggleDisplay = sunriseRun ? "Sunrise Run" : "Sunset Run" 
+
+
+    const today = new Date();
+    const date = today.getFullYear() + '-' + 0+(today.getMonth()+1) + '-' + 0+today.getDate();
 
     return (
         <section className="form">
             <form onSubmit={handleSubmit} action="">
                 <label htmlFor="date" className="sr-only">date</label>
-                <input onChange={handleChange} type="date" name="date" id="date" placeholder="yyyy-mm-dd" />
+                <input onChange={handleChange} type="date" name="date" id="date" placeholder="yyyy-mm-dd" min={date}/>
                 <a onClick={handleToggle}>{toggleDisplay}</a>
                 {sunriseRun === false && <>
                 <label className="sr-only">Length of run</label>
