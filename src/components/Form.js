d4c0491f2eb2d2877fb8a46ced15adb38537c8d2
chelseycornejo <chelsey.daguio@gmail.com>
2023-04-03 20:51:58 -0400

    added styling for form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
+import { FiSunrise } from 'react-icons/fi'
+import { FiSunset } from 'react-icons/fi'
 
 const Form = (props) => {
     // de-structuring the props and using the passed down functions for onSubmit and onChange events.
     const {handleChange, handleSubmit, handleToggle, sunriseRun} = props;
 
-    const [curDate, setCurDate] = useState(Date());
-    const toggleDisplay = sunriseRun ? "Sunrise Run" : "Sunset Run" 
+    const toggleDisplay = sunriseRun ? <><FiSunrise /><span className="sr-only">sunrise</span></> : <><FiSunset /><span className="sr-only">sunset</span></>
 
 
     const today = new Date();
@@ -13,7 +14,8 @@ const Form = (props) => {
 
     return (
         <section className="form">
-            <form onSubmit={handleSubmit} action="">
+            {/* WITHOUT labels, remember to updated _form.scss */}
+            {/* <form onSubmit={handleSubmit} action="">
                 <label htmlFor="date" className="sr-only">date</label>
                 <input onChange={handleChange} type="date" name="date" id="date" placeholder="yyyy-mm-dd" min={date}/>
                 <a onClick={handleToggle}>{toggleDisplay}</a>
@@ -22,6 +24,24 @@ const Form = (props) => {
                 <input type="numbers" placeholder="enter run length in minutes"></input>
                 </>}
                 <button>Get Sun Data!</button> 
+            </form> */}
+
+            <form onSubmit={handleSubmit} action="">
+                <div className="formInput">
+                <label htmlFor="date">enter date</label>
+                <input onChange={handleChange} type="date" name="date" id="date" placeholder="yyyy-mm-dd" min={date} />
+                </div>
+                <div className="formInput">
+                <p>sunrise | sunset</p>
+                <a onClick={handleToggle}>{toggleDisplay}</a>
+                </div>
+                <div className="formInput">
+                {sunriseRun === false && <>
+                    <label>Run Length(min)</label>
+                    <input type="numbers" placeholder="enter run length in minutes"></input>
+                </>}
+                </div>
+                <button>Get Sun Data!</button>
             </form>
         </section>
     )
