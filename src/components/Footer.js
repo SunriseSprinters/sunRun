59b4c7c7cffe41731800e295e219fa419ed80db1
Paige Thai <paigegiathai@gmail.com>
2023-04-05 18:34:27 -0400

    removed .instructions, modified styling for footer and added media queries

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
         <footer>
-            <p>Powered by <a href="https://sunrise-sunset.org/api " target="_blank" rel="noopener noreferrer"><span className="sr-only">This opens up in a new tab</span>Sunset and Sunrise API</a> © Created at <a href="https://junocollege.com/" target="_blank" rel="noopener noreferrer"><span className="sr-only">This opens up in a new tab</span>Juno College of Technology</a></p>
+            <p>Powered by  <a href="https://sunrise-sunset.org/api " target="_blank" rel="noopener noreferrer"><span className="sr-only">This opens up in a new tab</span>  Sunset and Sunrise API</a> © Created at  <a href="https://junocollege.com/" target="_blank" rel="noopener noreferrer"><span className="sr-only">This opens up in a new tab</span>  Juno College of Technology</a></p>
         </footer>
     )
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,6 @@ import sunsetIcon from "../assets/sunset.png"
 
 const Home = () => {
 
-    const handleClick = () => {
-        console.log("hello it me")
-    }
-
-
     return (
         <main>
             <section className="homePage">
@@ -19,40 +14,10 @@ const Home = () => {
                         <h1 className="animate__animated animate__bounceInRight">SunRun</h1>
                     </div>
 
-                    {/* Modal expansion for smaller screen sizes */}
-                    <div className="modalContainer">
-                        <button className="openModal" onClick={handleClick}><AiOutlineExpandAlt />
-                            <span className="sr-only">Open for instructions</span>
-                        </button>
-
-                        <div className="modalPop">
-                            <div className="modalContent">
-                                <span className="closeModal"><AiOutlineClose /></span>
-                                <div className="textContainer">
-                                    <h2>Welcome to SunRun!</h2>
-                                    <p>Say goodbye to dull runs and hello to a more exciting workout experience with our app designed to help you plan a scenic route while keeping track of the sunrise and sunset times.</p>
-                                    
-                                    <ul className="instructions">
-                                        <li>Select the date you want to run.</li>
-                                        <li>Pick sunrise or sunset.</li>
-                                        <li>If you selected <em>sunset</em>, enter the duration of your run, but in minutes! </li>
-                                    </ul>
-                                    <p>Click the arrow below to start planning!</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* End of modalContainer div */}
-
                     <div className="textContainer">
                         <h2>Welcome to SunRun!</h2>
                         <p>Say goodbye to dull runs and hello to a more exciting workout experience with our app designed to help you plan a scenic route while keeping track of the sunrise and sunset times.</p>
                         
-                        <ul className="instructions">
-                            <li>Select the date you want to run.</li>
-                            <li>Pick sunrise or sunset.</li>
-                            <li>If you selected <em>sunset</em>, enter the duration of your run, but in minutes! </li>
-                        </ul>
                         <p>Click the arrow below to start planning!</p>
                     </div>
 
