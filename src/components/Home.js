48ecf3d710eb5978de2642ab50030576b85c22a6
Paige Thai <paigegiathai@gmail.com>
2023-04-04 23:30:06 -0400

    added styling for media queries and added modal pop out, still pending logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import { AiOutlineArrowDown, AiOutlineExpandAlt, AiOutlineClose } from 'react-icons/ai';
 import 'animate.css'
 import sunsetIcon from "../assets/sunset.png"
 
-// ASSETS
-import { AiOutlineArrowDown } from 'react-icons/ai';
-
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("hello it me")
+    }
+
+
     return (
         <main>
             <section className="homePage">
@@ -14,6 +18,31 @@ const Home = () => {
                         <img src={sunsetIcon} alt="sunset with birds icon" />
                         <h1 className="animate__animated animate__bounceInRight">SunRun</h1>
                     </div>
+
+                    {/* Modal expansion for smaller screen sizes */}
+                    <div className="modalContainer">
+                        <button className="openModal" onClick={handleClick}><AiOutlineExpandAlt />
+                            <span className="sr-only">Open for instructions</span>
+                        </button>
+
+                        <div className="modalPop">
+                            <div className="modalContent">
+                                <span className="closeModal"><AiOutlineClose /></span>
+                                <div className="textContainer">
+                                    <h2>Welcome to SunRun!</h2>
+                                    <p>Say goodbye to dull runs and hello to a more exciting workout experience with our app designed to help you plan a scenic route while keeping track of the sunrise and sunset times.</p>
+                                    
+                                    <ul className="instructions">
+                                        <li>Select the date you want to run.</li>
+                                        <li>Pick sunrise or sunset.</li>
+                                        <li>If you selected <em>sunset</em>, enter the duration of your run, but in minutes! </li>
+                                    </ul>
+                                    <p>Click the arrow below to start planning!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* End of modalContainer div */}
 
                     <div className="textContainer">
                         <h2>Welcome to SunRun!</h2>
