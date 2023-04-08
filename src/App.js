5fbb25c94bc9dc72f63388272b1101958d6a02fb
Roberto Jan <lsbk0391@gmail.com>
2023-04-08 12:12:20 -0400

    fixed some issues on sideBar

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ function App() {
   return (
     <div className={`App ${pageTheme}`}>
       <Header toggleTheme={toggleTheme} pageTheme={pageTheme}/>
-      <SavedRuns />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sunApp' element={<SunInfo />} />
